7fad3e20a5f2143c28ede0b51a45d0485e030d6a
Kevin Durbin <kevindurb@fastmail.com>
2025-10-06 16:03:21 -0600

    update kube-vip

---
+++ scripts/gen-kube-vip.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+VIP=192.168.42.1
+INTERFACE=enp1s0f0
+KVVERSION=$(curl -sL https://api.github.com/repos/kube-vip/kube-vip/releases | jq -r ".[0].name")
+
+kube-vip() {
+  docker run --network host --rm "ghcr.io/kube-vip/kube-vip:${KVVERSION}" "$@"
+}
+
+kube-vip manifest daemonset \
+    --interface $INTERFACE \
+    --address $VIP \
+    --inCluster \
+    --taint \
+    --controlplane \
+    --services \
+    --arp \
+    --leaderElection > ./apps/kube-system/kube-vip/resources/kube-vip.yml
