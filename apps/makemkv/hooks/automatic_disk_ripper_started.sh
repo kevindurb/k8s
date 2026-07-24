3d634d83017357fc134550e6846035a744c99395
Kevin Durbin <kevindurb@fastmail.com>
2026-07-24 08:46:35 -0600

    try to send emails for notifications

---
+++ apps/makemkv/hooks/automatic_disk_ripper_started.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+
+/config/hooks/send_email.sh "Automatic Disk Ripper Started" "Automatic Disk Ripper Started"
---
+++ apps/makemkv/hooks/send_email.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+SMTP_SERVER="smtp-relay-service.smtp-relay:25"
+FROM_EMAIL="beavercloud@fastmail.com"
+TO_EMAIL="kevindurb@fastmail.com"
+
+SUBJECT="${1:-SUBJECT_MISSING}"
+BODY="${2:-BODY_MISSING}"
+
+echo "Sending email to ${TO_EMAIL}..."
+
+curl --silent --show-error \
+     --url "smtp://${SMTP_SERVER}" \
+     --mail-from "${FROM_EMAIL}" \
+     --mail-rcpt "${TO_EMAIL}" \
+     --upload-file - <<EOF
+From: ${FROM_EMAIL}
+To: ${TO_EMAIL}
+Subject: ${SUBJECT}
+
+${BODY}
+EOF
+
+echo "Email sent successfully!"
